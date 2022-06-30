1aaa62205523c833f21116bd93f292b0a5a11154
Sean Duffy <kqrpnb@gmail.com>
2022-06-29 23:59:11 -0400

    Completed up to 29.7.3

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Milky photos';
+  image1 = 'https://images.unsplash.com/photo-1544200175-ca6e80a7b323?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2562&q=80';
+  image2 = 'https://images.unsplash.com/photo-1524240189020-77884edd5d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bWlsa3l8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60';
+  image3 = 'https://images.unsplash.com/photo-1630710601248-f310515aedf4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1pbGt5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['Armageddon', 'Oceans 11', 'The Bourne Identity', 'Marcel the Shell With Shoes On'];
 
    constructor() { }
 
